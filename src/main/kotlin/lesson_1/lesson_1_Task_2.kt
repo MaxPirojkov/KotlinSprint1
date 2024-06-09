09220e6d1d183a2a1c3ce878c1788322a0ce0c37
Maxim <maxim.pirozhkov@exness.com>
2024-06-09 23:18:47 +0300

    KS-1-2 add new variebles and print in console

---
+++ src/main/kotlin/lesson_1/lesson_1_Task_2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main(){
+    val numberOfOrders : Int = 75
+    val thanksForPurchase : String = "Спасибо за заказ в нашем магазине"
+
+    println(numberOfOrders)
+    println(thanksForPurchase)
+
+    var numberOfEmployee = 2000
+ //   println(numberOfEmployee)
+
+    numberOfEmployee = 1999
+    println(numberOfEmployee)
+
+}
