4e9fa0700776700a2cf779bf6f475342e6bdaa0c
Maxim <maxim.pirozhkov@exness.com>
2024-06-10 16:14:14 +0300

    KS-1-5 add const, fix name

--- src/main/kotlin/lesson_1/lesson_1_Task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_Task_5.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_1
 
-const val sumOfSecondsInSpace = 6480
-const val oneHourInSeconds = 3600
+const val ONE_HOUR_IN_SECONDS = 3600
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
+    val sumOfSecondsInSpace = 6480
 
-    val hours = sumOfSecondsInSpace / oneHourInSeconds
-    val minute = (sumOfSecondsInSpace % oneHourInSeconds) / 60
-    val seconds = sumOfSecondsInSpace % 60
+    val hours = sumOfSecondsInSpace / ONE_HOUR_IN_SECONDS
+    val minute = (sumOfSecondsInSpace % ONE_HOUR_IN_SECONDS) / SECONDS_IN_MINUTE
+    val seconds = sumOfSecondsInSpace % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, minute, seconds))
 }
