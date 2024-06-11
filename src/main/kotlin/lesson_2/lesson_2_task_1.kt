4c9d1707eb839ab4616e4dd9a53c701a8152e16b
Maxim <maxim.pirozhkov@exness.com>
2024-06-11 21:47:08 +0300

    fix after review

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -5,12 +5,9 @@ fun main(){
     val student2 = 4.0
     val student3 = 3.0
     val student4 = 5.0
-
-
     val sumOfStudent = 4
 
     val result = ((student1 + student2 + student3 + student4) / sumOfStudent)
-    println(result::class) //вывод double
-    println(String.format("%02d", result)) // ошибка d != java.lang.Double, не смог понять почему
 
+    println(String.format("%.2f", result))
 }
