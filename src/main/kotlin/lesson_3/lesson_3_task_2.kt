f6e1c4518d325b0a7e99f815610b8efe1d9bdc21
Maxim <maxim.pirozhkov@exness.com>
2024-06-13 22:42:20 +0300

    KS-3-2 fix after review

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_3
 
 fun main(){
-    val yearsOldBeforeMarried = 20
-    val yearsOldAfterMarried = 22
-    val maidenName = "Андреева"
     val name = "Татьяна"
     val middleName = "Сергеевна"
-    val husbandSurname = "Сидорова"
+    var surname = "Андреева"
+    var yearsOld = 20
 
-    println("$maidenName $name $middleName, $yearsOldBeforeMarried")
-    println("$husbandSurname $name $middleName, $yearsOldAfterMarried")
+    println("$surname $name $middleName, $yearsOld")
+
+    surname = "Сидорова"
+    yearsOld = 22
+
+    println("$surname $name $middleName, $yearsOld")
 }
