4d647e08b8e2dbe51b641609658152b8e93dc7bd
Maxim <maxim.pirozhkov@exness.com>
2024-06-14 19:15:03 +0300

    KS-3-3

--- src/main/kotlin/lesson_3/lesson_3_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_3.kt
@@ -1,10 +1,15 @@
 package org.example.lesson_3
 
 fun main() {
+
+    val builder = StringBuilder()
     val number = 9
     var sum: Int = 0
+    
     for(i in 1 ..9){
         sum = number * i
-        println("$number * $i = $sum")
+        builder.append("$number * $i = $sum \n")
     }
+
+    println(builder.toString())
 }
