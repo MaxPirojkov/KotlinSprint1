c19a6835841aa28b84264e3837d2caa6b743024f
Maxim <maxim.pirozhkov@exness.com>
2024-06-17 18:19:59 +0300

    KS-3-3

--- src/main/kotlin/lesson_3/lesson_3_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_3.kt
@@ -2,14 +2,19 @@ package org.example.lesson_3
 
 fun main() {
 
-    val builder = StringBuilder()
-    val number = 9
-    var sum: Int = 0
-    
-    for(i in 1 ..9){
-        sum = number * i
-        builder.append("$number * $i = $sum \n")
-    }
+    val numb = 9
 
-    println(builder.toString())
+    val multipleTable = """
+        |$numb * 1 = ${numb * 1}
+        |$numb * 2 = ${numb * 2}
+        |$numb * 3 = ${numb * 3}
+        |$numb * 4 = ${numb * 4}
+        |$numb * 5 = ${numb * 5}
+        |$numb * 6 = ${numb * 6}
+        |$numb * 7 = ${numb * 7}
+        |$numb * 8 = ${numb * 8}
+        |$numb * 9 = ${numb * 9}
+        """.trimMargin()
+
+    println(multipleTable)
 }
